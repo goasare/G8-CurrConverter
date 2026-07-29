1442e64d95d16970b6fd42b0de98217f3643f677
Godfrey Asare <GodfreyOAsare@gmail.com>
2026-07-29 01:29:40 -0400

    Fix currency column to allow null for Other, add dark theme and display font

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    currency = db.Column(db.String(3), nullable=False)
+    currency = db.Column(db.String(3), nullable=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.currency}')"
